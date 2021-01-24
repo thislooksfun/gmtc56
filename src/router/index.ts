e9b5fa9eb3cca6060649996ab8f524fd8ca04b6b
thislooksfun <thislooksfun@repbot.org>
2021-01-24 16:03:40 -0600

    First commit

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: "gmtc56",
+  routes,
+});
+
+export default router;
---
+++ vue.config.js
@@ -0,0 +1,1 @@
+module.exports = { publicPath: "/gmtc56" };
