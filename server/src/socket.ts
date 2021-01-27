09fbcab48604ac04c630d369c93d88b685a5b03d
thislooksfun <thislooksfun@repbot.org>
2021-01-27 14:25:57 -0600

    Only allow logged in users to open websockets

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -10,6 +10,21 @@ export function init(server: Server, sessionParser: RequestHandler) {
 
   wss = new WebSocket.Server({ clientTracking: false, noServer: true });
 
+  server.on("upgrade", function(request, socket, head) {
+    // @ts-ignore
+    sessionParser(request, {}, () => {
+      if (!request.session.auth) {
+        socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+        socket.destroy();
+        return;
+      }
+
+      wss.handleUpgrade(request, socket, head, function(ws) {
+        wss.emit("connection", ws, request);
+      });
+    });
+  });
+
   wss.on("connection", ws => {
     // TODO: Get startup status.
     console.log("Got websocket connection!");
