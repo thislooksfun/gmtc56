d9124eaeb84ca96d14932b4a604c687bec1f52eb
thislooksfun <thislooksfun@repbot.org>
2021-01-28 01:50:27 -0600

    Make logo responsive

--- client/src/components/Logo.vue
+++ client/src/components/Logo.vue
@@ -1,21 +1,28 @@
 <template>
-  <img
-    class="gmtc-logo mx-auto"
+  <v-img
+    class="gmtc-logo my-8 mx-auto"
+    width="100%"
+    max-width="225"
+    aspect-ratio="1"
     alt="Great Midwest Trivia Contest 56"
     src="../assets/gmtc56-logo.png"
-  />
+  >
+  </v-img>
 </template>
 
 <script>
+import { VImg } from "vuetify/lib";
+
 export default {
   name: "Logo",
+  components: {
+    VImg,
+  },
 };
 </script>
 
 <style>
 .gmtc-logo {
-  width: 25vw;
-  height: 25vw;
   border-radius: 50%;
   margin: 30px 0;
 }
